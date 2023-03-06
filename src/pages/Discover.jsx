a17481f214d59ee043ca465f0df81ff749599867
Gurleen <gurleenwadhwa@dal.ca>
2023-03-06 17:02:56 -0400

    Added the basic music playing feature.

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import {Error, Loader, SongCard} from '../components'; 
 import {genres} from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+    const dispatch = useDispatch();
+    const {activeSong, isPlaying} = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
 
@@ -25,13 +29,16 @@ const Discover = () => {
         </div>
 
         
-        {/* Our Song card template */}
+        {/* Our Song card component */}
         
         <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
             {data?.map((song,i) => (
                 <SongCard
                     key={song.key}
                     song={song}
+                    isPlaying={isPlaying}
+                    activeSong={activeSong}
+                    data={data}
                     i={i}
                 />
             ))}

35596d30105dfe88be0bce96ee06a0a0b248bce8
Gurleen <gurleenwadhwa@dal.ca>
2023-03-06 15:06:38 -0400

    Connected the APi with hooks and implemeted the songcard component to display artists, their tracks, and albumArts.

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,7 +6,9 @@ const Discover = () => {
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
 
-    if(isFetching) return <Loader title="Loading Songs"/>
+    if(isFetching) return <Loader title="Loading Songs..."/>
+
+    if(error) return <Error/>;
     
     return(
     <div className='flex flex-col'>
@@ -26,7 +28,7 @@ const Discover = () => {
         {/* Our Song card template */}
         
         <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-            {[1,2,3,4,5,6,7,8,9,10].map((song,i) => (
+            {data?.map((song,i) => (
                 <SongCard
                     key={song.key}
                     song={song}
